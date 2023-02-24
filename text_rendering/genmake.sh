7f5c6446f797fd6fccc1a5a4974317818373fb3f
Alessandro Mauri <alemauri001@gmail.com>
2023-02-24 15:48:02 +0100

    aa

---
+++ text_rendering/genmake.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+genobj() {
+	#echo "$1" | sed -e 's/^/obj\//' -e 's/\.c/\.o/'
+	echo "$1" | sed -e 's/\.c/\.o/'
+}
+
+genrule() {
+	#gcc -MM "$1" | sed 's/^/obj\//'
+	gcc -MM "$1"
+}
+
+
+mkdir -p obj
+rm -f objlist
+
+cat > Makefile << EOF
+CC      = gcc
+LDFLAGS = -lm -lgrapheme -lSDL2 -lGLEW -lGL
+CFLAGS  = -g -Wall -Wextra -pedantic
+
+.PHONY: clean all
+all: test
+
+EOF
+
+for f in *.c; do
+	genrule $f >> Makefile
+	genobj $f >> objlist
+done
+
+mainrule='test: '
+linkcmd='	${CC} ${LDFLAGS} -o test '
+cleanrule='clean: 
+	rm -f '
+while IFS="" read -r line; do
+	mainrule="$mainrule $line"
+	linkcmd="$linkcmd $line"
+	cleanrule="$cleanrule $line"
+done < objlist
+
+echo "$mainrule"  >> Makefile
+echo "$linkcmd"   >> Makefile
+echo "$cleanrule" >> Makefile
