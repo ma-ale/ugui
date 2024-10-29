39e78ea0783fd0ea284f47c989645e19ac3d7138
Alessandro Mauri <alemauri001@gmail.com>
2024-10-29 22:45:47 +0100

    ported rewrite2 to c3

--- fm/libconf.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIBCONF_H_
-#define LIBCONF_H_
-
-const char *lcnf_get_conf_file(const char *id, const char *name, int global);
-const char *lcnf_get_conf_dir(const char *id, int global);
-
-#endif
